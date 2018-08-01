91a9cdd454f8479bbb32c2955d557c1539bf570c
Samantha Gatt <smfgatt@gmail.com>
2018-08-01 14:47:42 -0400

    Creates project and memory object

---
+++ Memories/Memories/Memory.swift
@@ -0,0 +1,15 @@
+//
+//  Memory.swift
+//  Memories
+//
+//  Created by Samantha Gatt on 8/1/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+struct Memory: Equatable, Codable {
+    var title: String
+    var body: String
+    var imageData: Data
+}
