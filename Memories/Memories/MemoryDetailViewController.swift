ece6ffb75e0edbc0940118c6b0295ff4dc223c39
Samantha Gatt <smfgatt@gmail.com>
2018-08-01 16:11:12 -0400

    Finishes storyboards

---
+++ Memories/Memories/MemoryDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by Samantha Gatt on 8/1/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func addPhoto(_ sender: Any) {
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+}
